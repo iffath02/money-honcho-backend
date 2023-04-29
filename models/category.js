c38fb804ac699469bb528ffde067f8bdce23e812
iffath02 <iffathfatima02@gmail.com>
2023-04-29 20:31:31 +1000

    category model added

--- models/category.js
+++ models/category.js
@@ -5,4 +5,9 @@ class Category {
     const sql = "select * from category;"
     return db.query(sql).then(res => res.rows)
   }
+
+  static create(name) {
+    const sql = "insert into category (name) values ($1) returning *;"
+    return db.query(sql, [name]).then(res => res.rows[0])
+  }
 }
