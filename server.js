204b126e33a3215f0f4ff123acd8fc3eb6b1fc03
iffath02 <iffathfatima02@gmail.com>
2023-06-01 13:20:21 +1000

    cors policy updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://react-money-honcho.onrender.com/",
+      "https://react-money-honcho.onrender.com",
     ],
     // methods: ["GET", "POST", "PUT", "DELETE"],
   })
